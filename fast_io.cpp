31c367aa64b49d5f5d6c722fea6d2bd5a8a36366
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-24 15:44:03 +0600

    For fast input & output

---
+++ fast_io.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // For Fast Input & Output
+    
+    return 0;
+}
